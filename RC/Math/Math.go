eb78c35842f62c08da2fcbc87266180f49681609
JaG <37165479+xtabs12@users.noreply.github.com>
2018-12-27 23:17:02 +0700

    Update Math.go

--- RC/Math/Math.go
+++ RC/Math/Math.go
@@ -5,6 +5,19 @@ import (
 )
 
 //Factorial Function
+func FactoriInt(value int) int {
+	var result int
+	result = 1
+	if value > 15 {
+		fmt.Printf("%i\n","error!!! -> Overload")
+		return 1
+	} else {
+		for i:=int(0);i<=value-1;i++ {
+			result = result * (i+1)
+		}
+		return result
+	}
+}
 func FactoriInt32(value int32) int32 {
 	var result int32
 	result = 1
@@ -35,4 +48,17 @@ func FactoriInt64(value int64) int64 {
 	return 1	
 }
 //end Factorial Function
+func Permutation(a int,b int) int {
+	nfac := FactoriInt(a)
+	nfmr := FactoriInt(a-b)
+	Result := nfac / nfmr
+	return Result
+}
+func Combination(a int,b int) int {
+	nfac := FactoriInt(a)
+	nfmr := FactoriInt(a-b)
+	nfnr := FactoriInt(b)
+	Result := nfac / (nfnr*nfmr)
+	return Result
+}
 
